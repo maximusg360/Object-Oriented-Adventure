f5e1c0bbcf8514bb78c9ebdeff7defede111fc3e
Guzman <francisco.guzman@accenture.com>
2024-06-24 20:17:20 -0400

    commit pt1

---
+++ app.js
@@ -0,0 +1,13 @@
+const adventurer = {
+    name: "Robin",
+    health: 10,
+    inventory: ["sword", "potion", "artifact"],
+    companion: {
+      name: "Leo",
+      type: "Cat",
+      companion: {
+        name: "Frank",
+        type: "Flea",
+      },
+    },
+  };
